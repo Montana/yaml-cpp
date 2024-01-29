bd3ef07c54a6be16e7e994166c62aa09f79bf474
John Scott <156694149+IderaJohn@users.noreply.github.com>
2024-01-29 11:37:29 -0500

    Update build.bat - \ to /

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 echo Hello world!
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe" build\YAML_CPP.sln
+"C:/Program Files (x86)/Microsoft Visual Studio/2017/Professional/MSBuild/15.0/Bin/MSBuild.exe" build/YAML_CPP.sln
