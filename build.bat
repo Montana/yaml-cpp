ca126c095d68857fd0cdce744ca2be9908dc4571
John Scott <156694149+IderaJohn@users.noreply.github.com>
2024-02-07 11:40:41 -0500

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-"C:/Program Files (x86)/Microsoft Visual Studio/2017/BuildTools/MSBuild/15.0/Bin/MSBuild.exe" ./build/YAML_CPP.sln /property:Configuration=%1
+"C:/Program Files (x86)/Microsoft Visual Studio/2017/BuildTools/MSBuild/15.0/Bin/MSBuild.exe" ./build/YAML_CPP.sln /property:Configuration=%1 /property:Platform=%2
